7d5f149784f7b11f19949a1d8d613e712237ce5c
AndriiShnurkov <Shans12.84@ukr.net>
2020-09-24 13:33:54 +0300

    added lesson 1

---
+++ Module_1/lesson_1/index.js
@@ -0,0 +1,267 @@
+// ----------------------- prompt confirm -------------------------------
+// const name = prompt("Enter your name, please!");
+// const age = prompt("Enter your age, please!");
+
+// console.log("Name:", name, " Age:", age);
+// console.log("Name:", name, " Age:", age);
+
+
+// ======================= types =======================================
+
+
+// ----------------- Number ---------------------
+// const age = 5;
+// console.log(age);
+
+// --------------------- string -------------------
+
+// const name = prompt("Enter your name:");
+// const book = prompt('Enter book'); 
+
+// const sentence = `${name} reed ${book}`;
+
+// console.log(sentence);
+
+
+// ---------------------------- example ------------------
+// let number = 0;
+// let sum = 0;
+
+// for (let i = 0; i < 10; i += 1) {
+//     number = prompt("Enter your number:");
+//     sum = sum + Number(number);
+
+//     console.log(`Your number: ${number}, current sum: ${sum}`)
+// }
+
+// console.log(`Total sum: ${sum}`);
+
+// ---------------------------- boolean -----------------------
+// const isAuthenticated = false; //true || false
+
+
+
+// --------------------- example ----------------------
+// const isAuthenticated = true;
+
+// if (isAuthenticated) {
+//     console.log("Hello")
+// }
+
+// isAuthenticated && console.log("Hello")
+
+// console.log(isAuthenticated);
+
+// console.log(`isAuthenticated: ${isAuthenticated}`)
+
+// ----------------------------- null ---------------------------------
+// const value = null;
+// -------------------------------------------------------------------
+
+// ----------------------------- undefined ---------------------------
+
+// let name;
+
+// console.log(name);
+
+// name = "Alex";
+
+// console.log(name);
+
+// --------------------------------------------------------------------
+// let name = "";
+// let number = 0;
+// let sum = 0;
+
+// --------------- ex ----------------------
+// let result = 0;
+// const PI = 3.14;
+
+// let number = prompt("Enter number");
+
+// if (name === '') {
+//     number = 0;
+// } else result = Number(number) + PI;
+
+
+// console.log(result);
+
+// ------------------------- typeof ------------------------
+// const age = 5;
+// const name = "Alex111";
+
+
+// console.log(typeof name)
+
+// const result = null
+// const result = [{name: "alex"}];
+// console.log(typeof result)
+
+// console.log(typeof age);
+// console.log(typeof name);
+
+
+// -------------------------- prompt confirm ---------------------------------
+
+// const input = prompt("Enter value"); //type string || null
+// console.log(input)
+// console.log(typeof input);
+
+
+// ------------------------- example --------------------
+// const input = prompt("Enter radius");
+// if (input === null) {
+//     console.log("Отменено пользователь");
+// } else console.log(`Вы ввели: "${input}"`);
+
+// --------------------------------------------------
+
+// const a = "5";
+// const b = 10;
+// const d = 25;
+
+// console.log(a > 5)
+// console.log(a < 5)
+// console.log(a <= 5)
+// console.log(a === 5);
+// console.log(5 != 5);
+// console.log(5 !== 5);
+
+// --------------------------------
+// let name = 5; //undefined, null, 0, -0, false, "" //falsy
+
+// if (name) {
+//     console.log(`Hello ${name}`)
+
+// } else console.log("No name")
+
+// const name = "fsdfdsfd";
+// let price = 1000
+
+// let message = `Выбран ${name}, цена за штуку ${price} кредитов`;
+// console.log(message)
+// price = 2000;
+
+// message = `Выбран ${name}, цена за штуку ${price} кредитов`;
+// console.log(message)
+
+
+// let price = 200
+// for (let i = 0; i < 5; i += 1) {
+//     price += 100
+//     let message = `Выбран i, цена за штуку ${price + 100} кредитов`;
+//     console.log(message)
+// }
+
+
+// console.log(`Выбран "Генератор защитного поля", цена за штуку 2000 кредитов`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let isAgree = confirm("Are you agree with terms..");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
