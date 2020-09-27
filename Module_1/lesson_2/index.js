dac9a456da422280727ec911fbaa1d2c5e50841c
AndriiShnurkov <Shans12.84@ukr.net>
2020-09-27 22:32:17 +0300

    added lesson2

---
+++ Module_1/lesson_2/index.js
@@ -0,0 +1,316 @@
+console.log("Lesson_2")
+
+// ----------- Numbers -----------------
+
+
+// let totalSum = 10;
+// let number = 0;
+
+// while (number !== null) {
+//     number = prompt("dfhmsjf");//"5"
+//     if (number) { //undefined, "", null, false, 0 ,-0, NaN
+//         // totalSum += Number(number);
+//         // totalSum = totalSum + Number(number);
+//         totalSum -= Number(number);
+//         totalSum = totalSum - Number(number);
+//     }
+// }
+
+// console.log(totalSum);
+
+// const value = "4.56px438798";
+// console.log(parseInt(value));
+// console.log(parseFloat(value));
+
+// // window.console.log("Hello")
+// // console.dir(window)
+
+
+// const a = 0.000223232;
+// const b = 0.99999999999999999999999999999999999999;
+// console.log("b", b)
+// // console.log(a + b);
+// // console.log((a * 1000000000 + b * 1000000000) / 1000000000);
+
+// console.log(0.1 + 0.2)
+// console.log((0.1 * 10 + 0.2 * 10) / 10)
+
+// MATH
+
+// const a = 5;
+// const b = 10;
+// const c = 15;
+// const d = 25;
+
+// const maxValue = Math.max(a, b, c, d);
+// const minValue = Math.min(a, b, c, d);
+// console.log(maxValue)
+
+// const randomValue = Math.random() * 100;
+// console.log(randomValue);
+
+// const result = Math.floor(1.5);
+// const result = Math.ceil(1.5);
+// console.log(result);
+
+
+// const user = {
+//     name: "Alex",
+//     status: "student",
+//     course: "BC2"
+// }
+
+
+
+// ------------ String -----------------
+// const str = "Hello";
+
+// let value = "Alex"
+// console.log(str + " " + value);
+
+// value = "Nikita"
+// console.log(str + " " + value);
+
+
+// text-indent: 25px;
+
+
+// ---------
+
+
+// const str = new String("Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, consequuntur.")
+// console.log(str)
+
+// const str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, consequuntur.";
+
+// console.dir(str.toUpperCase());
+
+// if (str.includes("sit")) {
+//     console.log("includes")
+//     console.log(str.indexOf('sit'));
+// }
+// console.log("sit".length);
+
+// const value = "Alex"
+
+// console.log(`Hello ${value + '!'}`);
+
+
+// -------------- logical operators -----------
+
+
+// И  -  &&;
+
+
+// const isAuthenticated = true;
+// const isOnline = true;
+
+// if (isAuthenticated === true) {
+//     console.log('isAuthenticated', isAuthenticated)
+// }
+
+// isAuthenticated && isOnline && console.log("Hello");
+
+// if (isAuthenticated === true) {
+//     if (isOnline === true) {
+//         console.log(text);
+//     }
+// }
+// ------------------------------------
+
+// или - ||
+
+
+// const isAuthenticated = false;
+// const isOnline = true;
+// const text = "Hello Alex!";
+
+// // (isAuthenticated || isOnline)
+// //     && console.log(text);
+
+// // if (isAuthenticated === false) {
+// if (!isAuthenticated) {
+//     console.log("please sign in");
+
+// } else if (isOnline === true) {
+//     console.log(text)
+// }
+
+
+// const isOnline = null;
+// console.log(!!isOnline);
+
+// ---------------------------------------------------------
+
+// const isAuth = false;
+// const tag = document.querySelector('.title');
+//     (isAuth) ? tag.textContent = "Hello" : tag.textContent = "By";
+
+// ----------------- if -----------
+// const a = 5;
+// const b = 10;
+
+
+// if (a < b) {
+//     console.log("true")
+// }
+// ----------------- if -----------
+// const a = 10;
+// const b = 10;
+
+
+// if (a > b) {
+//     console.log("a > b")
+// } else console.log("a < b")
+
+
+// if (a > b) {
+//     console.log("a > b");
+// } else {
+//     console.log("a < b");
+//     console.log("Ghjgh");
+// }
+
+// if (a > b) {
+//     console.log("a > b");
+// } else if (a < b) {
+//     console.log("a < b");
+// } else if (a === b) {
+//     console.log("a === b")
+// }
+
+
+// ----------------------------------
+// const number1 = prompt("number1");//"3"
+// const number2 = prompt("number2"); //"5"
+// const operation = prompt("operation"); //"*"
+
+// let result = 0;
+
+// if (number1 !== null && number2 !== null && operation !== null) {
+//     const a = Number(number1); //"3" => 3
+//     const b = Number(number2); //"5" => 5
+
+//     switch (operation) {
+//         case "+":
+//             result = a + b
+//             break;
+//         case "-":
+//             result = a - b
+//             break;
+//         case "*":
+//             result = a * b
+//             break;
+//         case "/":
+//             result = a / b
+//             break;
+//         case "%":
+//             result = a % b
+//             break;
+//         default:
+//             console.log("No result")
+//             break;
+//     }
+//     console.log(result);
+// }
+
+// -------------------------
+
+// const num = 3;
+// console.log(num);
+// const strNum = `${num}`;
+// console.log(strNum);
+
+
+// ------------------------------------------------
+
+
+// let a = 5;
+// console.log(a)
+
+
+// if (true) {
+//     let a = 10;
+//     console.log(a)
+// }
+
+
+// if (true) {
+//     let a = 15;
+//     console.log(a)
+// }
+
+// console.log(a);
+
+
+// function adfs() {
+//     let a = 5;
+// }
+
+
+// const JS = {
+//     global: {
+//         a: 5,
+//         b: 10,
+
+//         block: {
+//             a: 10,
+//         },
+
+//         functions: {
+
+//         }
+//     },
+
+
+// }
+
+// ---------------------
+
+// const isAuth = false;
+// // const isActive = true;
+// const text1 = "Hello";
+// const text2 = "By"
+
+// isAuth ? console.log(text1) : console.log(text2);
+
+// if (isAuth === true) {
+//     console.log(text1)
+// } else console.log(text2)
+
+// ----------------------------
+
+// let isOpen = false;
+
+// isOpen ? console.log("open") : console.log("close");
+
+// isOpen = true;
+
+// isOpen ? console.log("open") : console.log("close");
+
+// isOpen = false;
+
+// isOpen ? console.log("open") : console.log("close");
+
+// =================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
